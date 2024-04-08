dad4f73ae35539567a3162dabf703f72d12fcb3f
Conrado Vargas <conradovargas93@gmail.com>
2024-04-07 23:36:29 -0300

    Models y Clases creadas

---
+++ src/ar/edu/unju/fi/model/Alumno.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.model;
+
+public class Alumno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author maco_
+ *
+ */
+module sistema_de_notas {
+}
